00fd11d3c35f828345ea92d066d7784d3fbdd564
Ignacio Gonzalez Lesiu <ignaciogonzalezlesiu@gmail.com>
2024-11-13 03:00:07 -0300

    Agregado middlewares

--- router.php
+++ router.php
@@ -1,11 +1,15 @@
 <?php
-session_start(); // iniciamos sesion
+require_once 'app/middlewares/verifySession.php';
+require_once 'app/middlewares/verifyAuth.php';
+require_once 'libs/response.php';
 require_once 'app/controllers/shirt.controller.php';
 require_once 'app/controllers/auth.controller.php';
 // base_url para redirecciones y base tag
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
-$action = 'home'; // accion por defecto si no se envia ninguna
+$res = new Response();
+
+$action = 'list'; // accion por defecto si no se envia ninguna
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
@@ -15,11 +19,13 @@ if (!empty($_GET['action'])) {
 $params = explode('/', $action);
 
 switch ($params[0]) {
-    case 'home':
+    case 'list':
+        sessionAuthMiddleware($res);
         $controller = new shirtController();
         $controller->showShirts();
         break;
     case 'description':
+        sessionAuthMiddleware($res);
         $controller = new shirtController();
         $controller->showDetail($params[1]);
         break;
@@ -32,22 +38,32 @@ switch ($params[0]) {
         $controller->login();
         break;
     case 'modifyItems':
+        sessionAuthMiddleware($res);
+        verifyAuthMiddleware($res);
         $controller = new shirtController();
         $controller->showCrud();
         break;
     case 'add':
+        sessionAuthMiddleware($res);
+        verifyAuthMiddleware($res);
         $controller = new shirtController();
         $controller->addShirt();
         break;
     case 'delete':
+        sessionAuthMiddleware($res);
+        verifyAuthMiddleware($res);
         $controller = new shirtController();
         $controller->deleteShirt($params[1]);
         break;
     case 'modify':
+        sessionAuthMiddleware($res);
+        verifyAuthMiddleware($res);
         $controller = new shirtController();
         $controller->showUpdate($params[1]);
         break;
     case 'update':
+        sessionAuthMiddleware($res);
+        verifyAuthMiddleware($res);
         $controller = new shirtController;
         $controller->modifyShirt();
         break;
